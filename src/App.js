71a1699f33e2ebc8b8106a338bd89d4159f68fe9
necmigunduz <necmigunduz@gmail.com>
2022-09-26 10:13:42 +0300

    Add React setup with TailwindCSS and Fontawesome dynamic with Babel

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { solid } from '@fortawesome/fontawesome-svg-core/import.macro' // <-- import styles to be used
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className='bg-slate-100'>Title</h1>
+      <FontAwesomeIcon icon={solid('coffee')} />
     </div>
   );
 }
